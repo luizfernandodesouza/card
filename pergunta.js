74b6fd27ccb34834c46cfbab8fef3de6d5a40735
luizfernandodesouza <152321819+luizfernandodesouza@users.noreply.github.com>
2024-09-30 13:56:00 -0300

    30/09

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+'progamação',
+'O que é python?',
+'O python é uma linguagem de progamação'
+)
+
+criaCartao(
+    'geografia',
+    'qual a capital da França?',
+    'A capital da França é paris'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'Uma funçãp é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+'Lingua inglesa',
+'Como se diz oi inglês?',
+'O'
+)
